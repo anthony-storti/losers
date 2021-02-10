529e67b076fb8709f5df44db8ec558f13c0d90eb
Stephen Holt Carlson <stephen.carlson@uvm.edu>
2021-02-09 19:22:42 -0500

    continuing pseudocode

---
+++ main.py
@@ -0,0 +1,17 @@
+# function to take a string input and break it up into a list of components, being mindful of quotation marks
+def inputToList(input):
+
+
+#function to validate input for errors, typos, that kinda thing
+def validateinput(input):
+
+#function to print commands
+def printHelp():
+
+
+# function to identify individual terms as tables, columns, etc
+
+
+#function to run the querys
+def do_query(x,y,z):
+
