e4f99ca7f14010c786f239eebde0b320580be3fb
Anthony.Storti <anthony.storti@uvm.edu>
2021-02-10 21:54:25 -0500

    fetch and return statements for single table queries done and ready for testing

--- parser.py
+++ parser.py
@@ -2,8 +2,8 @@ import data
 
 data.init_db()
 cur = data.sqliteConnect()
-print(data.fetch(cur, "Insults", "date", "insult", "weak"))
-
+print(data.fetch(cur, "Insults", "date", "insult", "dummy"))
+print(data.fetch(cur, "Insults", "date", "insult", "dummy", "Loser"))
 
 
 
