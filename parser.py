9e8337f6f3f5a6d55f138b350664cd2592e179b5
Zunyi Liao <zliao2@uvm.edu>
2021-02-12 18:29:46 -0500

    updating the csv files and add some functionalities to the parser.py

--- parser.py
+++ parser.py
@@ -1,9 +1,32 @@
 import data
+import subprocess
+import os
+import sys
 
-data.init_db()
 cur = data.sqliteConnect()
-print(data.fetch(cur, "dummy", "Insults", "date", "insult"))
-print(data.fetch(cur, "Rick-Perry"))
+
+a = sys.argv[1]
+
+
+def command():
+    data.init_db()
+
+    name = str(a)
+
+    print(data.fetch(cur, "dummy", "Insults", "date", "insult"))
+    print(data.fetch(cur, name))
+
+    cur.commit()
+    drop_tables()
+
+
+def drop_tables():
+    cur.commit()
+    cur.close()
+    subprocess.call("rm data.db")
+
+
+command()
 
 
 
